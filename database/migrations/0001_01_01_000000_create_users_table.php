4daf6d29d0fc10a75340bde10ea5a32177a7bfc4
Azaan Ali Malik <197883@novacollege.nl>
2026-04-15 14:47:42 +0200

    Refactor users table migration to include additional fields: lastname, address, city, postal_code, phone_number, and role.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('role')->required();
             $table->rememberToken();
             $table->timestamps();
         });
