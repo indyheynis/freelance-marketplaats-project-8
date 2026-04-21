e9b274bf9bf30de2e5e64d1d833e807586556c6c
Azaan Ali Malik <197883@novacollege.nl>
2026-04-21 13:16:54 +0200

    Add dashboard controller with methods for freelancer and client views

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+  public function freelancer()
+    {
+        /** @var User $user */
+        $user = Auth::user();
+
+        if (!$user->isFreelancer()) {
+            return redirect()->route('dashboard.client');
+        }
+
+        return view('dashboard.freelancer');
+    }
+
+    public function client()
+    {
+        /** @var User $user */
+        $user = Auth::user();
+
+        if (!$user->isClient()) {
+            return redirect()->route('dashboard.freelancer');
+        }
+
+        return view('dashboard.client');
+    }
+}
