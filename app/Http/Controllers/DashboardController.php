8243fc4e15410acc189076f143b37cdb6bc4e0a7
Azaan Ali Malik <197883@novacollege.nl>
2026-04-21 13:34:18 +0200

    Add separate dashboard views for freelancers and clients with routing

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-  public function freelancer()
+     public function freelancer()
     {
         /** @var User $user */
         $user = Auth::user();
