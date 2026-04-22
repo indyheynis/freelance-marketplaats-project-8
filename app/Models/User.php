4be56cba4dbe62596e475f8e02e8d066d9574e4b
Hidde Manz <hiddemanz@gmail.com>
2026-04-22 09:02:29 +0200

    User model changed

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Commission;
 
 #[Fillable([
     'name',
@@ -43,5 +44,10 @@ class User extends Authenticatable
     public function isClient(): bool
     {
         return $this->role === 'client';
+    }
+
+    public function commissions()
+    {
+        return $this->hasMany(Commission::class);
     }
 }
