0490868c392a09f8711877b89000279f0a58fd23
kaio <198046@novacollege.nl>
2026-05-12 13:36:26 +0200

    Remove redundant conflict marker from Commission model

--- app/Models/Commission.php
+++ app/Models/Commission.php
@@ -26,7 +26,6 @@ class Commission extends Model
         return $this->hasMany(Application::class);
     }
 
-<<<<<<< HEAD
     public function reviews()
     {
         return $this->hasMany(Review::class);
